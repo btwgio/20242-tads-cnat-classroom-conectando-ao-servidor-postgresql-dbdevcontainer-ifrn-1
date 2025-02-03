facb6ebe15ed5ef82ae8393bc798390516deff9a
Giovanna <130415868+btwgio@users.noreply.github.com>
2025-02-03 17:46:43 -0300

    Update agendamento.sql

--- exercicios/agendamento.sql
+++ exercicios/agendamento.sql
@@ -1,13 +1,14 @@
 CREATE TABLE Pessoa (
-    nome_completo VARCHAR(100) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    cpf CHAR(11) PRIMARY KEY,
     data_nascimento DATE NOT NULL,
-    endereco VARCHAR(30),
+    endereco VARCHAR(300) NOT NULL,
     telefone VARCHAR(15)
+    CONSTRAINT unique_email_nome UNIQUE (email, nome)
 );
 
 CREATE TABLE Especialidade (
     id SERIAL PRIMARY KEY, 
-    descricao VARCHAR(150) NOT NULL 
+    descricao VARCHAR(100) NOT NULL 
 )
